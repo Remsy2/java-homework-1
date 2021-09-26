b68a21631e7140783e9cd7b4f410ae44c763c0f7
Виталий Пашин <white2shark@mail.ru>
2021-09-26 19:17:14 +0300

    java-homework-1

--- src/application/Application.java
+++ src/application/Application.java
@@ -4,8 +4,23 @@ import model.Kotik;
 public class Application {
     public static void main(String[] args) {
 
-        Kotik kotik = new Kotik();
-        kotik.eat("Murzik");
+        Kotik cat1=new Kotik("Pushok", 13,12,"meeeeow");
+        Kotik cat2=new Kotik();
+        cat2.setKotik("Snezhok", 12, 11, "meeeeeowwwww");
 
+
+        if (cat2.getVoice()==cat1.getVoice()) {
+            System.out.println("Their voice is the same");
+        }
+        else {
+            System.out.println("Their voice is NOT the same");
+
+        }
+        System.out.println("Your cat's name is "+cat2.getName());
+        System.out.println("Your cat's weight is "+cat2.getWeight());
+
+        cat2.liveAnotherDay();
+        cat2.showCounter();
     }
+
 }
--- src/model/Kotik.java
+++ src/model/Kotik.java
@@ -2,62 +2,166 @@ package model;
 
 public class Kotik {
 
-    private int prettiness;
-    private double weight;
+    private static int counter=0;
+    private int bellyfulLevel=10;
+
     private String name;
+    private int weight;
+    private int prettiness;
     private String meow;
-    private boolean satietyOfTheCat;
-    private
+    private String eatenFood;
 
-    public Kotik(int prettiness, double weight, String name, String meow, boolean satietyOfTheCat) {
-        this.prettiness = prettiness;
-        this.weight = weight;
-        this.name = name;
-        this.meow = meow;
-        this.satietyOfTheCat = satietyOfTheCat;
+
+
+    public Kotik() { //constructor with no parameter
+        counter++;
+
+    }
+    public Kotik(String name, int weight,int prettiness, String meow){  //constructor with all necessary parameters
+        this.name=name;
+        this.weight=weight;
+        this.prettiness=prettiness;
+        this.meow=meow;
+
+        counter++;
     }
 
-    public int getPrettiness() {
-        return prettiness;
+    public void setKotik(String name, int weight,int prettiness, String meow) {
+        this.name=name;
+        this.weight=weight;
+        this.prettiness=prettiness;
+        this.meow=meow;
     }
 
-    public double getWeight() {
-        return weight;
-    }
 
     public String getName() {
-        return name;
+        return this.name;
+
+    }
+    public String getFoodName() {
+        return this.eatenFood;
+
+    }
+    public int getWeight() {
+        return this.weight;
+
+    }
+    public String getVoice() {
+        return this.meow;
+    }
+    public int getPrettiness() {
+        return this.prettiness;
+
     }
 
-    public String getMeow() {
-        return meow;
+    public void eat(int eaten) { //method with one parameter
+        bellyfulLevel+=eaten;
     }
 
-    public double getSatietyOfTheCat() {
-        return satietyOfTheCat;
+    public void eat(int eaten, String foodName) { //overloaded method with two parameter
+        bellyfulLevel+=eaten; //increase bellyful level to given parameter
+        this.eatenFood=foodName;
+
+    }
+    public void eat() { //method with no parameter calling overloaded method
+        this.eat(1,"Milk");
     }
 
-    public boolean eat(String name) {
-        if (getSatietyOfTheCat() == false) {
-            this.name = name;
-            System.out.println("Котик не сытый. Нужно покормить!");
-        } else {
-            this.name = name;
-            System.out.println("Котик сытый!");
+    public boolean sleep(){
+        if (bellyfulLevel<=0) { //first it checks did the cat eat
+            System.out.println("Your cat is hungry, give it some food...");
+            return false; //if not them returns false
+        }
+        else {
+            System.out.println("Your cat has eaten so it can sleep");
+            bellyfulLevel--; //decrease bellyfulLEvel to one because action takes energy
+            return true;
+        }
 
+
+    }
+
+    public boolean play() {
+        if (bellyfulLevel<=0) {
+            System.out.println("Your cat is hungry, give it some food...");
+            return false;
+        }
+        else {
+
+            System.out.println("Your cat has eaten so it can play");
+            bellyfulLevel--;
+            return true;
         }
     }
 
-//
-//        private void sayMeow () {
-//            System.out.println(name + "Say " + meow);
-//
-//        }
+    public boolean chaseMouse() {
+        if (bellyfulLevel<=0) {
+            System.out.println("Your cat is hungry, give it some food...");
+
+            return false;
+        }
+        else {
+            System.out.println("Your cat has eaten so it can chase mouse");
+            bellyfulLevel--;
+            return true;
+        }
+    }
+    public boolean makeMess(){
+        if (bellyfulLevel<=0) { //first it checks did the cat eat
+            System.out.println("Your cat is hungry, give it some food...");
+            return false; //if not them returns false
+        }
+        else {
+            System.out.println("Your cat has eaten so it can make a mess");
+            bellyfulLevel--; //decrease bellyfulLEvel to 1 because action takes energy
+            return true;
+        }
 
 
-//        public boolean sleep () {
-//            System.out.print("Sleeping zz-z-z-z...");
-//            return sleep;
-//        }
-//
+    }
+    public boolean doCrap(){
+        if (bellyfulLevel<=0) { //first it checks did the cat eat
+            System.out.println("Your cat is hungry, give it some food...");
+            return false; //if not them returns false
+        }
+        else {
+            System.out.println("Your cat has eaten so it can do a crap");
+            bellyfulLevel--; //decrease bellyfulLEvel to one because action takes energy
+            return true;
+        }
 
+
+    }
+
+    public void liveAnotherDay() {
+        for (int i=1; i<=24; i++) { //loop for
+            int methodNo=(int)(Math.random()*6 + 1);//method number
+
+            switch(methodNo) {
+                case 1:
+                    this.sleep();
+                    break;
+                case 2:
+                    this.play();
+                    break;
+                case 3:
+                    this.doCrap();
+                    break;
+                case 4:
+                    this.makeMess();
+                    break;
+                case 5:
+                    this.chaseMouse();
+                    break;
+                case 6:
+                    this.eat();
+                    break;
+            }
+
+        }
+
+    }
+    public void showCounter() {  // method which shows number of objects created
+        System.out.println(counter +" objects are created");
+    }
+}
